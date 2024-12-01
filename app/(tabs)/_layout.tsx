8e8ef294681c9e9a4577c3a284336d68052a615a
Vasu Garg <vasug20@gmail.com>
2024-11-30 20:20:23 -0600

    enhances the layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,31 +1,54 @@
+import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from 'expo-router';
+import { View, Platform } from 'react-native';
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: 'green',
+        tabBarShowLabel: false, // Hide labels for all tabs
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-            title: 'Map',
-            tabBarIcon: ({ color }) => <FontAwesome size={24} color={color} />,
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="map" size={24} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="Microphone"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View
+              style={{
+                width: 60,
+                height: 60,
+                borderRadius: 30,
+                backgroundColor: 'white',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: Platform.OS === 'android' ? 20 : 30,
+              }}
+            >
+              <FontAwesome name="microphone" size={30} color="green" />
+            </View>
+          ),
+          tabBarLabel: '',
         }}
       />
 
       <Tabs.Screen
         name="Review"
         options={{
-            title: 'Review',
-            tabBarIcon: ({ color }) => <FontAwesome size={24} color={color} />
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="star" size={24} color={color} />
+          ),
         }}
-      />
-
-      <Tabs.Screen
-      name="Microphone"
-      options={{
-        title: 'Microphone',
-        tabBarIcon: ({ color }) => <FontAwesome size={24} color={color} />
-      }}
       />
     </Tabs>
   );
